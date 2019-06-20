5ece0a7a349cc04144980a515e9108e3c60a63d9
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-06-21 01:13:20 +0700

    added some kinds of stuff

---
+++ components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Logo">
+    <div class="ikanaki"/>
+  </div>
+</template>
+
+<style>
+.Logo {
+  height: auto;
+  width: 350px;
+  margin: 17px auto;
+}
+
+.ikanaki {
+  background-repeat: no-repeat;
+  height: 350px;
+  background-image: url("../assets/ikanaki.png");
+  background-size: cover;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo/>
+      <h1 class="title">We’re performing an awesome app at the moment.</h1>
+      <h2 class="subtitle">So stay tuned!</h2>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 40px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 32px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
